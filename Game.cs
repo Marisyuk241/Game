afb9fd920b28ddbdd5a55d8ece9682a2742d732b
Marisyuk241 <90959444+Marisyuk241@users.noreply.github.com>
2021-09-18 20:02:52 +0300

    Add files via upload

---
+++ Game.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using cod;
+namespace cod
+{
+    
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            short thislo;
+            bool a = true;
+            Game game = new Game();
+            Console.WriteLine("Комп`ютор загадал число УГАДАЙ?????");
+            for (; a;)
+            {
+            //  Console.WriteLine(game.Number);
+
+                thislo = Convert.ToInt16(Console.ReadLine());
+
+                if (game.Get_right(thislo))
+                {
+                    a = false;
+                    Console.WriteLine("victory");
+                }
+                else
+                {
+                    game.Number_of_attempts--;
+                    if (game.Number_of_attempts < 1)
+                    {
+                        Console.WriteLine(game.Number);
+                        game.Reset();
+                        Console.WriteLine(" All over again");
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿ using System;
+    using System.IO;
+    using System.Linq;
+    using System.Collections.Generic;
+    using cod;
+    namespace cod
+    {
+        public class Game
+        {
+            private short number_of_attempts = 5;
+            private int number;
+            public Game()
+            {
+                Random rand = new Random();
+                number = rand.Next(1, 100);
+            }
+            public short Number_of_attempts
+            {
+                set { number_of_attempts = value; }
+                get { return number_of_attempts; }
+            }
+            public int Number
+            {
+                set { number = value; }
+                get { return number; }
+            }
+            public void Reset()
+            {
+                Random rand = new Random();
+                number = rand.Next(1, 100);
+                number_of_attempts = 5;
+            }
+            public bool Get_right(short n)
+            {
+                if (n < number)
+                {
+                    Console.WriteLine("the number is greater");
+                }
+                if (n > number)
+                {
+                    Console.WriteLine("number less");
+                }
+                return n == number;
+            }
+        }
+    }
